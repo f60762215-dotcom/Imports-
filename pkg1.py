304e7340120252b2dd839f2592b18ede52d9a8ca
f60762215-dotcom <f60762215@gmail.com>
2026-01-04 23:53:30 +0330

    pkg1.py

--- pkg1.py
+++ pkg1.py
@@ -1,3 +1,11 @@
+import time
 import tkinter as tk
-e = tk.Tk()
-e.mainloop()
+
+def create_page(name, size_x, size_y):
+  e = tk.Tk()
+  e.title(f"{name}")
+  e.geometry(f"{size_x}x{size_y}")
+  e.mainloop()
+if __name__ == '__main__':
+  print("wellcome to my pkg farzad")
+  print("commands: create_page(name, size_x, size_y")
